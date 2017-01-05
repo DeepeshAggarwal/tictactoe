af3ac71c4d13168518496a9ffad615eb53f00018
Deepesh Aggarwal <m431959@a4370559.federated.fds>
2017-01-05 11:05:38 -0800

    making tictactoe more responsive

--- js/nextMove.js
+++ js/nextMove.js
@@ -1,85 +1,93 @@
 var CHOICE = [-1, -1];
-function gameOver(player, board, depth){
-    if( board[0] == board[3] && board[3] == board[6] && board[0] != '_'){
-        if (board[0] == player)
-            return 10-depth
-        else
-            return depth-10
+
+function isThreeSame(board, x, y, z) {
+    if (board[x] == board[y] && board[y] == board[z] && board[x] != '_') {
+        return true;
     }
-    else if( board[1] == board[4] && board[4] == board[7] && board[1] != '_'){
+    return false;
+}
+
+function gameOver(player, board, depth) {
+    if (isThreeSame(board, 0, 3, 6)) {
+        if (board[0] == player)
+            return 10 - depth
+        else
+            return depth - 10
+    }
+    else if (isThreeSame(board, 1, 4, 7)) {
         if (board[1] == player)
-            return 10-depth
+            return 10 - depth
         else
-            return depth-10
+            return depth - 10
     }
-    else if( board[2] == board[5] && board[5] == board[8] && board[2] != '_'){
+    else if (isThreeSame(board, 2, 5, 8)) {
         if (board[2] == player)
-            return 10-depth
+            return 10 - depth
         else
-            return depth-10
+            return depth - 10
     }
-    else if(board[0] == board[1] && board[1] == board[2] && board[0] != '_'){
+    else if (isThreeSame(board, 0, 1, 2)) {
         if (board[0] == player)
-            return 10-depth
+            return 10 - depth
         else
-            return depth-10
+            return depth - 10
     }
-    else if( board[3] == board[4] && board[4] == board[5] && board[3] != '_'){
+    else if (isThreeSame(board, 3, 4, 5)) {
         if (board[3] == player)
-            return 10-depth
+            return 10 - depth
         else
-            return depth-10
+            return depth - 10
     }
-    else if( board[6] == board[7] && board[7] == board[8] && board[6] != '_'){
+    else if (isThreeSame(board, 6, 7, 8)) {
         if (board[6] == player)
-            return 10-depth
+            return 10 - depth
         else
-            return depth-10
+            return depth - 10
     }
-    else if( board[0] == board[4] && board[4] == board[8] && board[0] != '_'){
+    else if (isThreeSame(board, 0, 4, 8)) {
         if (board[0] == player)
-            return 10-depth
+            return 10 - depth
         else
-            return depth-10
+            return depth - 10
     }
-    else if( board[2] == board[4] && board[4] == board[6] && board[2] != '_'){
+    else if (isThreeSame(board, 2, 4, 6)) {
         if (board[2] == player)
-            return 10-depth
+            return 10 - depth
         else
-            return depth-10
+            return depth - 10
     }
     else {
         for (var i = 2; i >= 0; i--)
             for (var j = 2; j >= 0; j--)
-                if(board[3*i+j] == '_')
+                if (board[3 * i + j] == '_')
                     return undefined
         return 0
     }
 }
-function nextMoveHelper(player, board, depth, row, column, isPlayerTurn){
+function nextMoveHelper(player, board, depth, row, column, isPlayerTurn) {
     var scores = new Array();
     var moves = new Array();
     // console.log(board)
-    if(row >= 0 && column >= 0){
-        board[3*row + column] = player
-        if(!isPlayerTurn && player == 'X')
-            board[3*row + column] = 'O'
-        else if(!isPlayerTurn && player == 'O')
-            board[3*row + column] = 'X'
+    if (row >= 0 && column >= 0) {
+        board[3 * row + column] = player
+        if (!isPlayerTurn && player == 'X')
+            board[3 * row + column] = 'O'
+        else if (!isPlayerTurn && player == 'O')
+            board[3 * row + column] = 'X'
     }
     score = gameOver(player, board, depth)
-    if(score != undefined)
+    if (score != undefined)
         return score
     // range = Array.apply(null, Array(5)).map(function (_, i) {return i;});            
-    for (var i = 2; i >= 0; i--) 
-        for (var j = 2; j >= 0; j--) 
-            if(board[3*i + j] == '_'){
+    for (var i = 2; i >= 0; i--)
+        for (var j = 2; j >= 0; j--)
+            if (board[3 * i + j] == '_') {
                 scores.push(nextMoveHelper(player, JSON.parse(JSON.stringify(board)), depth + 1, i, j, !isPlayerTurn));
                 moves.push([i, j]);
             }
 
     //print scores, moves
-    if(!isPlayerTurn){
+    if (!isPlayerTurn) {
         max_score = Math.max.apply(null, scores);//max(scores)
         max_index = scores.indexOf(max_score)
         CHOICE = moves[max_index]
@@ -95,8 +103,16 @@ function nextMoveHelper(player, board, depth, row, column, isPlayerTurn){
     }
     console.log(CHOICE);
 }
-function nextMove(player,board){
+function nextMove(player, board) {
     nextMoveHelper(player, board, 0, -1, -1, false);
     console.log(CHOICE);
     return CHOICE
+}
+
+function isGameOver() {
+    var score = gameOver('X', board(), 0);
+    if (score !== undefined) {
+        return true;
+    }
+    return false;
 }
